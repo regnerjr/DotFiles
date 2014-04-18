a12ade7375fb1dd9dde10947a5d1384cdcb67b44
John Regner <j.regner@astronautics.com>
2014-04-18 10:35:29 -0500

    Updated setup script to also symlink .liquidpromptrc if liquidprompt is installed, and to symlink the bash_logout file

--- setup.sh
+++ setup.sh
@@ -7,3 +7,17 @@ if [ -f $HOME/.bashrc ] ; then
 fi
 rm $HOME/.bashrc
 ln --symbolic $PWD/.bashrc $HOME/.bashrc
+
+if [[ -d $HOME/liquidprompt ]] ; then
+    printf "Looks like you have liquid prompt installed."
+    printf "Symlinking .liquidpromptrc"
+    rm $HOME/.liquidpromptrc
+    ln --symbolic $PWD/.liquidpromptrc $HOME/.liquidpromptrc
+fi
+
+if [[ -f $HOME/.bash_logout ]] ; then
+    rm $HOME/.bash_logout
+    printf "Symlinking new .bash_logout"
+fi
+
+ln --symbolic $PWD/.bash_logout $HOME/.bash_logout
