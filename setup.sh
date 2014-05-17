42a71af72c8c61967ba1f967f6017617fb3e4299
John Regner <john@johnregner.com>
2014-05-17 11:16:08 -0500

    OS X does not allow the --symbolic flag to go with ln. Shortened to -s

--- setup.sh
+++ setup.sh
@@ -6,23 +6,23 @@ if [ -f $HOME/.bashrc ] ; then
     read ENTER
 fi
 rm $HOME/.bashrc
-ln --symbolic $PWD/.bashrc $HOME/.bashrc
+ln -s $PWD/.bashrc $HOME/.bashrc
 
 if [[ -d $HOME/liquidprompt ]] ; then
     printf "Looks like you have liquid prompt installed."
     printf "Symlinking .liquidpromptrc"
     rm $HOME/.liquidpromptrc
-    ln --symbolic $PWD/.liquidpromptrc $HOME/.liquidpromptrc
+    ln -s $PWD/.liquidpromptrc $HOME/.liquidpromptrc
 fi
 
 if [[ -f $HOME/.bash_logout ]] ; then
     rm $HOME/.bash_logout
     printf "Symlinking new .bash_logout"
-    ln --symbolic $PWD/.bash_logout $HOME/.bash_logout
+    ln -s $PWD/.bash_logout $HOME/.bash_logout
 fi
 
 
 if [[ $SHELL == '/bin/zsh' ]]; then
     printf "Horray ZSH. Linking .zshrc"
-    ln --symbolic $PWD/.zshrc       $HOME/.zshrc
+    ln -s $PWD/.zshrc       $HOME/.zshrc
 fi
